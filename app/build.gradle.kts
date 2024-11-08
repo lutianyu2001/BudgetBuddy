aacea8d2eb1ab972acd81dc6ba0224d03660b5f7
Tianyu (Sky) Lu <tlu83@wisc.edu>
2024-11-08 15:48:43 -0600

    build bug fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.cs407.budgetbuddy"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cs407.budgetbuddy"
-        minSdk = 33
-        targetSdk = 34
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
